54ed3250bb33e65e4aa7f11d114f47a8f498e0a3
Robert Dumas <dumasrobert13@gmail.com>
2023-01-23 15:45:06 +0800

    active program is changing when tapped

--- lib/pages/home/widgets/activity.dart
+++ lib/pages/home/widgets/activity.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class RecentActivities extends StatelessWidget {
@@ -6,9 +8,98 @@ class RecentActivities extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Expanded(
+        child: Padding(
+      padding: const EdgeInsets.symmetric(
+        vertical: 20,
+        horizontal: 10,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Recent Activities',
+            style: Theme.of(context).textTheme.headline1,
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: 10,
+              itemBuilder: (context, index) => ActivityItem(),
+            ),
+          )
+        ],
+      ),
+    ));
+  }
+}
+
+class ActivityItem extends StatelessWidget {
+  const ActivityItem({super.key});
+
+  static const activities = [
+    'Running',
+    'Swimming',
+    'Hiking',
+    'Walking',
+    'Cycling',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    String activity = activities[Random().nextInt(activities.length)];
+
+    return GestureDetector(
+      onTap: () {
+        Navigator.of(context).pushNamed('/details');
+      },
       child: Container(
-        width: double.infinity,
-        color: Color(0xFF2196F3),
+        margin: const EdgeInsets.symmetric(vertical: 5),
+        height: 50,
+        decoration: BoxDecoration(
+            border: Border.all(
+              color: Color(0xffe1e1e1),
+            ),
+            borderRadius: BorderRadius.circular(10)),
+        child: Row(children: [
+          SizedBox(width: 10),
+          Container(
+            padding: const EdgeInsets.all(4),
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: Color(0xffcff2ff),
+            ),
+            height: 35,
+            width: 35,
+            child: Container(
+              decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                    image: AssetImage('assets/running.jpg'),
+                    fit: BoxFit.fill,
+                  )),
+            ),
+          ),
+          SizedBox(width: 10),
+          Text(
+            activity,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w900,
+            ),
+          ),
+          Expanded(child: SizedBox()),
+          Icon(Icons.timer, size: 12),
+          Text(
+            '30min',
+            style: TextStyle(fontSize: 10, fontWeight: FontWeight.w300),
+          ),
+          SizedBox(width: 10),
+          Icon(Icons.wb_sunny_outlined, size: 12),
+          Text(
+            '55kcal',
+            style: TextStyle(fontSize: 10, fontWeight: FontWeight.w300),
+          ),
+          SizedBox(width: 20),
+        ]),
       ),
     );
   }
